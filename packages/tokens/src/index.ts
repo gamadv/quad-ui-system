c7fc55c17d0d69b8485edde81231b4a9ab4ea190
gamadv <lealdev.py@gmail.com>
2023-09-03 16:05:52 -0300

    Creating packages: Tokens

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,9 @@
+export * from './colors'
+
+export * from './spaces'
+export * from './radio'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
