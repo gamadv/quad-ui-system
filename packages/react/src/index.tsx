458d2c6e6711fdb7f2146e49d1eb9ffed4488f63
Gamadv <lealdev.py@gmail.com>
2024-04-08 18:55:43 -0300

    feat: Create react styles with Stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,11 @@
-export default function index() {
-  return <div>index</div>
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  height: '',
+})
+
+export function index() {
+  return <Button>Test my button</Button>
 }
