d7ec6f26f71a9923d09db6a72a8a0d96d87fa5a0
Gamadv <lealdev.py@gmail.com>
2024-05-05 20:53:02 -0300

    feat(SB): Create Tokens Grid Component ;reusable for all tokens

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,31 @@
+import '../styles/tokens-grid.css'
+
+interface TokensGridProps {
+  tokens: Record<string, string>
+  hasRemValue?: boolean
+}
+
+export function TokensGrid(props: TokensGridProps) {
+  const { tokens, hasRemValue } = props
+  return (
+    <table className="tokens-grid">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+              {hasRemValue && <td>{Number(value.replace('rem', '')) * 16}</td>}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
