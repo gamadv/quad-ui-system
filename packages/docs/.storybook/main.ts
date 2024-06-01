dd13b55421712e6981e1d1f4e35fab7346a7253e
Gamadv <lealdev.py@gmail.com>
2024-06-01 14:50:29 -0300

    📝 chore(SB): Add A11y addon && phosphor deps

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -23,6 +23,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/manager-api'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
