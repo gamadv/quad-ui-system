311a492a3cc39ef8b7a9a664e43741b4d7b8f610
Gamadv <lealdev.py@gmail.com>
2024-06-02 16:38:14 -0300

    👷 Update preview file type to solve pages error

---
+++ packages/docs/.storybook/preview.tsx
@@ -0,0 +1,21 @@
+import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming'
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark
+    }
+  },
+};
+
+export default preview;
