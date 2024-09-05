3bb31cd7150758134f1fd2a0e065d00b170a38b1
Gamadv <lealdev.py@gmail.com>
2024-09-05 11:13:50 -0300

    🐛 fix(React): Change TextInputProps to fix size props error

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,14 +1,14 @@
 import { ComponentProps, forwardRef, ElementRef } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
-
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
+  containerProps?: ComponentProps<typeof TextInputContainer>
 }
 
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
-  ({ prefix, ...props }: TextInputProps, ref) => {
+  ({ prefix, containerProps, ...props }, ref) => {
     return (
-      <TextInputContainer>
+      <TextInputContainer {...containerProps}>
         {!!prefix && <Prefix>{prefix}</Prefix>}
         <Input ref={ref} {...props} />
       </TextInputContainer>
